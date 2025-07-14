cdbb6dc74b003f9ea6f93e4ad10d66c6f9267e45
Cấn Thái Sơn <zoubis2001@gmail.com>
2025-07-14 14:04:01 +0700

    Update footer content to remove contact information and retain project title

--- web-client-dev/src/Layout/MainLayout/Footer.tsx
+++ web-client-dev/src/Layout/MainLayout/Footer.tsx
@@ -10,9 +10,7 @@ const Footer = () => {
         />
         <div className="text-right">
           <div>Bản quyền thuộc về Khoa Điện tử viễn thông, Đại học Bách khoa Hà Nội</div>
-          <div>Văn phòng: Văn phòng: C1 - 320 - Quận Hai Bà Trưng - Hà Nội</div>
-          <div>Điện thoại: 024 3869 6211  & 024 3623 1478</div>
-          <div>Email: seee@hust.edu.vn</div>
+          <div>Đồ án 1</div>
         </div>
       </div>
     </div>
