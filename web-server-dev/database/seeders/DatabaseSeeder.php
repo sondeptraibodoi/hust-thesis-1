f3fce187dca407dd0713aff4a05828968a13ac08
Cấn Thái Sơn <zoubis2001@gmail.com>
2025-07-14 08:48:04 +0700

    Add command to generate application key in setup instructions

--- web-server-dev/database/seeders/DatabaseSeeder.php
+++ web-server-dev/database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            // CreateTestDataSeeder::class,
-            // MonHocSeeder::class,
+            CreateTestDataSeeder::class,
+            MonHocSeeder::class,
             DeThiSeeder::class
         ]);
     }
